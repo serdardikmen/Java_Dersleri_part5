ff7aeb831c04d72922efdfb1be703e8aabdc9ccc
serdardikmen <60195944+serdardikmen@users.noreply.github.com>
2020-08-04 16:44:16 +0300

    Add files via upload

---
+++ ders5/AccountTest.java
@@ -0,0 +1,17 @@
+package javadersleri.ders5;
+
+public class AccountTest {
+	
+	public static void main(String [] args) {
+	
+
+	
+	Account account2 = new Account("1234567895",1000.0,"Serdarhan Dikmen","serdar.d3277@gmail.com","4546484");
+	
+	account2.paraYatir(100);
+	
+	
+	
+	
+	}
+}
